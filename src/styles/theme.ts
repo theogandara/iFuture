e11b7790e62bd1781dd42a31a81b5751e2b983ef
Théo Gândara <theogandara@gmail.com>
2022-09-07 19:19:39 -0300

    Initial Configs

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    background: '#FFF',
+    primary: '#3351EE',
+    text: '#100F18',
+    textWhite: '#FFF',
+    secondary: '#1835CA',
+    error: '#DA1751',
+    disabled: '#D7DDFB',
+    placeholder: '#74768A'
+  }
+}
+
+export default theme
